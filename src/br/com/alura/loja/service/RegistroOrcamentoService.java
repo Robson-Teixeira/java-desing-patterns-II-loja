f091027b971a2bc9f70020b5a71be9bfe89ec0be
Robson-Teixeira <robsonmteixeira@outlook.com>
2024-01-02 11:10:37 -0300

    Adapters para reutilizar dependências: API de registro de orçamento

---
+++ src/br/com/alura/loja/service/RegistroOrcamentoService.java
@@ -0,0 +1,16 @@
+package br.com.alura.loja.service;
+
+import br.com.alura.loja.modelo.Orcamento;
+
+public class RegistroOrcamentoService {
+
+	public void registrar(Orcamento orcamento) {
+
+		// Chamada HTTP para a API externa
+		// URL Connection
+		// Http Client
+		// lib Rest
+
+	}
+
+}
